8d8ac04c5d489b2db5321c1f9d5e1a9720ccd79e
code-navigator <markbdean1@gmail.com>
2023-05-05 20:22:00 -0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+div
+  q-splitter(
+    v-model="splitterModel"
+    style="height: 100vh"
+  )
+
+    template(
+      v-slot:before
+    )
+      div.q-pa-md
+        div.text-h4.q-mb-md Before
+        div(
+          v-for="n in 20" 
+          :key="n" 
+          class="q-my-md"
+        ) {{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.
+
+    template(
+      v-slot:after
+    )
+      q-splitter(
+        v-model="insideModel"
+        horizontal
+      )
+
+        template(
+          v-slot:before
+        )
+          div.q-pa-md
+            div.text-h4.q-mb-md Before
+            div(
+              v-for="n in 20" 
+              :key="n" 
+              class="q-my-md") {{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.
+
+        template(
+          v-slot:after
+        )
+          div.q-pa-md
+            div.text-h4.q-mb-md After
+            div(
+              v-for="n in 20" 
+              :key="n" 
+              class="q-my-md"
+            ) {{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      splitterModel: ref(50), // start at 50%
+      insideModel: ref(50)
+    }
+  }
+}
+</script>
